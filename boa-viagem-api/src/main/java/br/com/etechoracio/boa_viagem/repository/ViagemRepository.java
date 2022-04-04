c54ea81814e93972deb092419613125825c08f03
Gabriel França <gabrielbfranca27@gmail.com>
2022-04-04 08:06:29 -0300

    JpaRepository da classe Gasto

--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/repository/ViagemRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.etechoracio.boa_viagem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ViagemRepository extends JpaRepository<Viagem, Long> {
-
-}
